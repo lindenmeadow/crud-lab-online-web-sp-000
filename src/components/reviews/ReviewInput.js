85cca3ddeb20b6a057540dea31623a9546667c1b
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 19:32:50 +0000

    Automatically backed up by Learn

--- src/components/reviews/ReviewInput.js
+++ src/components/reviews/ReviewInput.js
@@ -2,6 +2,30 @@ import React, { Component } from 'react';
 import Reviews from './Reviews';
 
 class ReviewInput extends Component {
+
+  state = {
+    text: ''
+  }
+
+  handleChange = (event) => {
+    this.setState(
+      {text: event.target.value}
+    )
+  }
+
+  handleSubmit = (event) =>{
+    event.preventDefault()
+    this.props.addReview({
+      text: this.state.text,
+      restaurantId: this.props.restaurantId
+
+    })
+
+    this.setState({
+      text: '',
+    });
+  }
+  
   render() {
     return (
       <div>
