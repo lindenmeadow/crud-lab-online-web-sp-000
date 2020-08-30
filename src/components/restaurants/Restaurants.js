087500a363339762a19b129ace7f7f38c3658962
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 19:30:21 +0000

    Automatically backed up by Learn

--- src/components/restaurants/Restaurants.js
+++ src/components/restaurants/Restaurants.js
@@ -3,6 +3,17 @@ import Restaurant from './Restaurant';
 
 class Restaurants extends Component {
   render() {
+
+    const restaurantsListed = this.props.restaurants.map(restaurant => {
+      return (
+        <Restaurant
+            key={restaurant.id}
+            restaurant={restaurant}
+            deleteRestaurant={this.props.deleteRestaurant}
+        />
+      )
+    });
+    
     return(
       <ul>
         Restaurants Component
