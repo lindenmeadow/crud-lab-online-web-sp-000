a421498b409824a480d06506766e9b802fdadb3e
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 19:38:55 +0000

    Automatically backed up by Learn

--- src/containers/ReviewsContainer.js
+++ src/containers/ReviewsContainer.js
@@ -15,4 +15,17 @@ class ReviewsContainer extends Component {
   }
 }
 
-export default ReviewsContainer
+const mapStateToProps = state => {
+  return {
+    reviews: state.reviews
+  }
+}
+
+const mapDispatchToProps = dispatch =>  {
+  return {
+      addReview: review => dispatch({type: 'ADD_REVIEW', review: review}),
+      deleteReview: id => dispatch({type: 'DELETE_REVIEW', id})
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ReviewsContainer)
