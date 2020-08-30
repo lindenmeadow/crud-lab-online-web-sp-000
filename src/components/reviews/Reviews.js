4c976bd7a3a0ec0ae37cffc93ead5ed538046657
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 20:05:33 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -2,25 +2,18 @@ import React, { Component } from 'react';
 import Review from './Review';
 
 class Reviews extends Component {
-  const specificReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
 
-  const listedReviews = specificReviews.map(review =>{
+  render() {
+    const specificReviews = this.props.reviews.filter(review => review.restaurantId == this.props.restaurantId)
+    const listedReviews = specificReviews.map(review =>{
       return (
         <Review
           key={review.id}
           review={review}
-          deleteReview={this.props.deleteReview}/>
+          deleteReview={this.props.deleteReview}
+        />
       )
-    }
-    )
 
-  render() {
-    return (
-      <ul>
-        Reviews
-        {listedReviews}
-      </ul>
-    );
   }
 };
 
