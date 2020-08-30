e25780564341cecfa5ffa41701fc5e8e274dfba4
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 20:02:13 +0000

    Automatically backed up by Learn

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,7 +3,7 @@ import Review from './Review';
 
 class Reviews extends Component {
   const specificReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
-  
+
   const listedReviews = specificReviews.map(review =>{
       return (
         <Review
