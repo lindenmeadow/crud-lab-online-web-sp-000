7b65f1694662b26957c20b31bf898d865493ce1e
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 19:29:07 +0000

    Automatically backed up by Learn

--- src/components/restaurants/RestaurantInput.js
+++ src/components/restaurants/RestaurantInput.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class RestaurantInput extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      text: ''
+    };
+  };
+
+
+  handleChange = (event) => {
+    this.setState({
+      text: event.target.value
+    });
+  };
+
+  handleSubmit = (event) =>{
+    event.preventDefault()
+    this.props.addRestaurant(this.state.text)
+  }
+  
   render() {
     return (
       <div>
