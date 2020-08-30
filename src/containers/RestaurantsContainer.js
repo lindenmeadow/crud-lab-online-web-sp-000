2d352936422a500bf400663677647444164a2142
Lyndel Evan Barnes <l.evan.barnes@gmail.com>
2020-08-30 19:35:57 +0000

    Automatically backed up by Learn

--- src/containers/RestaurantsContainer.js
+++ src/containers/RestaurantsContainer.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react'
 import RestaurantInput from '../components/restaurants/RestaurantInput'
 import Restaurants from '../components/restaurants/Restaurants'
+import { connect } from 'react-redux'
 
 class RestaurantsContainer extends Component {
 
   render() {
     return (
       <div>
-        <RestaurantInput />
-        <Restaurants />
+      <RestaurantInput addRestaurant={this.props.addRestaurant}/>
+      <Restaurants />	        
+      <Restaurants restaurants={this.props.restaurants} deleteRestaurant={this.props.deleteRestaurant}/>
       </div>
     )
   }
